a05b66c641f8f8b09b849c12c9800eb11debbbb5
Dallas Slaughter and Josh Linton <dallas@>
2017-02-28 13:20:35 -0800

    Add additional item, begin prototypes

--- items.js
+++ items.js
@@ -3,14 +3,24 @@
 
 
 
-var item = {
-  name: Sword;
+var items = [{
+  name: "Sword";
   slot: 1;
   damage: 10;
   armor: 0;
   altText: "it's a sword"; //alt text on hover?
-  image: "image.png"; //makes sense to reference this here
+  image: "sword.png"; //makes sense to reference this here
   stackable: false; //probably best to set this now, potions etc would be true
   value: 10; //gold value
   special: function() //heyo it's a method. Useful for magic items later
-}
+}, {
+  name: "Reflective Vest";
+  slot: 2;
+  damage: 0;
+  armor: 10;
+  altText: "Don't get hit by cars"; //alt text on hover?
+  image: "reflectiveVest.png"; //makes sense to reference this here
+  stackable: false; //probably best to set this now, potions etc would be true
+  value: 10; //gold value
+  special: function() //heyo it's a method. Useful for magic items later
+}]
--- player.js
+++ player.js
@@ -2,7 +2,7 @@
 
 function Player(name) = {
   this.playerName = name;
-  this.maxHealth = 16;
+  this.maxHealth = 64; // 1/4 of a carpet square = 4pts (extra to account for armor)
   this.currHealth = 16;
   this.equip = [item, item];
   this.inventory = [item, item, item, item, item];
@@ -10,9 +10,13 @@ function Player(name) = {
 }
 
 var newPlayer = new Player(inputName);
-//pick up item
- if (player.inventory.length === 5) {
-   //tell player inventory full
- } else (
-   //add item to inventory
- )
+
+
+Player.prototype.addItem(item, quantity) {
+  //pick up item
+   if (player.inventory.length === 5) {
+     //tell player inventory full
+   } else (
+     //add item to inventory
+   )
+}
